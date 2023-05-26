9b2f28bb5127d92a660568a49b6110d098c0793d
busy-gnl <vallantjesse@live.com>
2023-05-26 10:17:17 +0200

    made home page responsive

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -13,29 +13,27 @@ export default function Home() {
           src="../../../public/images/iss-bacteries.webp"
           alt="'ISS"
         />
-        <div>
-          <p className="intro-iss">
-            L'ISS, acronyme de International Space Station, est une station
-            spatiale en orbite terrestre basse. C'est un projet international
-            développé par la NASA, Roscosmos, l'Agence spatiale fédérale russe,
-            l'Agence spatiale européenne (ESA), l'Agence japonaise d'exploration
-            aérospatiale (JAXA) et l'Agence spatiale canadienne (ASC). <br />
-            <br />
-            L'ISS est un laboratoire en orbite qui permet la réalisation de
-            recherches scientifiques dans de nombreux domaines tels que la
-            biologie, la physique, l'astronomie, la médecine et les sciences de
-            la Terre. Elle est également utilisée pour tester de nouvelles
-            technologies et préparer les futures missions d'exploration
-            spatiale. <br />
-            <br />
-            La construction de l'ISS a commencé en 1998 et elle est devenue
-            opérationnelle en 2000. Elle est habitée en permanence par une
-            équipe internationale de astronautes qui effectuent des rotations
-            régulières. L'ISS est un exemple remarquable de coopération
-            internationale dans le domaine de l'exploration spatiale, et elle
-            joue un rôle important dans la recherche scientifique et le
-            développement de l'expérience humaine dans l'espace.
-          </p>
+        <div className="home-intro-iss">
+          L'ISS, acronyme de International Space Station, est une station
+          spatiale en orbite terrestre basse. C'est un projet international
+          développé par la NASA, Roscosmos, l'Agence spatiale fédérale russe,
+          l'Agence spatiale européenne (ESA), l'Agence japonaise d'exploration
+          aérospatiale (JAXA) et l'Agence spatiale canadienne (ASC). <br />
+          <br />
+          L'ISS est un laboratoire en orbite qui permet la réalisation de
+          recherches scientifiques dans de nombreux domaines tels que la
+          biologie, la physique, l'astronomie, la médecine et les sciences de la
+          Terre. Elle est également utilisée pour tester de nouvelles
+          technologies et préparer les futures missions d'exploration spatiale.{" "}
+          <br />
+          <br />
+          La construction de l'ISS a commencé en 1998 et elle est devenue
+          opérationnelle en 2000. Elle est habitée en permanence par une équipe
+          internationale de astronautes qui effectuent des rotations régulières.
+          L'ISS est un exemple remarquable de coopération internationale dans le
+          domaine de l'exploration spatiale, et elle joue un rôle important dans
+          la recherche scientifique et le développement de l'expérience humaine
+          dans l'espace.
         </div>
       </section>
       <h2>Date de lancement du premier module : 20 novembre 1998</h2>
