06df7433665b9a9e43d226bd50fbba6a4364a264
busy-gnl <vallantjesse@live.com>
2023-05-28 19:21:01 +0200

    added handleChange and handleSubmit to the contact form

--- frontend/src/components/ContactForm/ContactForm.jsx
+++ frontend/src/components/ContactForm/ContactForm.jsx
@@ -2,21 +2,71 @@ import React from "react";
 import "./ContactForm.css";
 
 export default function ContactForm() {
+  const [name, setName] = React.useState("");
+  const [email, setEmail] = React.useState("");
+  const [message, setMessage] = React.useState("");
+  const [isSubmitted, setIsSubmitted] = React.useState(false);
+
+  const handleChange = (event) => {
+    const fieldName = event.target.name;
+    const { value } = event.target;
+    switch (fieldName) {
+      case "name":
+        setName(value);
+        break;
+      case "email":
+        setEmail(value);
+        break;
+      case "message":
+        setMessage(value);
+        break;
+      default:
+        break;
+    }
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setIsSubmitted(true);
+    setName("");
+    setEmail("");
+    setMessage("");
+    setTimeout(() => {
+      setIsSubmitted(false);
+    }, 10000);
+  };
+
   return (
-    <form className="contact-form" action="">
+    <form className="contact-form" action="" onSubmit={handleSubmit}>
       <label className="contact-label" htmlFor="name">
         Votre nom
       </label>
-      <input className="contact-input" type="text" name="name" id="name" />
+      <input
+        className="contact-input"
+        onChange={handleChange}
+        value={name}
+        type="text"
+        name="name"
+        id="name"
+      />
       <label className="contact-label" htmlFor="email">
         Votre email
       </label>
-      <input className="contact-input" type="email" name="email" id="email" />
+      <input
+        className="contact-input"
+        onChange={handleChange}
+        value={email}
+        type="email"
+        name="email"
+        id="email"
+      />
       <label className="contact-label" htmlFor="message">
         Votre message
       </label>
       <textarea
         className="contact-input"
+        onChange={handleChange}
+        value={message}
         name="message"
         id="message"
         cols="30"
@@ -25,6 +75,9 @@ export default function ContactForm() {
       <button className="contact-button" type="submit">
         Envoyer
       </button>
+      {isSubmitted ? (
+        <div className="contact-message">Votre message à bien été envoyé!</div>
+      ) : null}
     </form>
   );
 }
