d9549839b066d68fd61caa59f89713debebdc9f7
busy-gnl <vallantjesse@live.com>
2023-06-25 23:43:40 +0200

    chat in progress

--- frontend/src/components/chat/Chat.jsx
+++ frontend/src/components/chat/Chat.jsx
@@ -1,54 +1,60 @@
-import { useState } from "react";
-import { Configuration, OpenAIApi } from "openai";
+import React, { useState } from "react";
+// import axios from "axios";
+// import { Configuration, OpenAIApi } from "openai";
 import "./Chat.css";
 
 export default function Chat() {
-  const configuration = new Configuration({
-    apiKey: "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",
-  });
-  const openai = new OpenAIApi(configuration);
+  const [chatInput, setChatInput] = useState("");
+  const [result, setResult] = useState();
 
-  const [message, setMessage] = useState("");
-  const [answer, setAnswer] = useState("");
+  async function onSubmit(event) {
+    event.preventDefault();
+    try {
+      const response = await fetch("https://api.openai.com/v1/completions", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer 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`,
+        },
+        body: JSON.stringify({
+          model: "text-davinci-003",
+          prompt: `Tu es l'un des membres de l'équipage de l'ISS, qui est composé de Sergueï Prokopyev, Frank Rubio, Dimitri Petelin, Stephen G. Bowen, Woody Hoburg, Andrey Fedvaev et Sultan Alneyadi. Tu communiques avec des fans de votre mission dans l'espace se trouvant sur terre via internet.\nFan : Bonjour ! Je suis tellement émerveillé de pouvoir discuter avec vous, Astronaute Alex, depuis la Station spatiale internationale (ISS). Comment ça va aujourd'hui ?\nAstronaute : Bonjour ! Je suis ravi de pouvoir échanger avec vous également. La vie dans l'espace est vraiment extraordinaire ! Nous flottons en microgravité, nous menons des expériences et nous contemplons la beauté de notre planète depuis ce point de vue unique. C'est une aventure constante !\nFan : ${chatInput}\nAstronaute:`,
+          max_tokens: 256,
+          temperature: 1,
+          top_p: 1,
+          frequency_penalty: 0,
+          stop: [" Fan:", " Astronaute:"],
+        }),
+      });
 
-  async function getAnswer() {
-    const response = await openai.createCompletion({
-      model: "text-davinci-003",
-      prompt: `The following is a conversation with an AI assistant. The assistant is helpful, creative, clever, and very friendly.\n\nHuman: Hello, who are you?\nAI: I am an AI created by OpenAI. How can I help you today?\nHuman: I'd like to cancel my subscription.\nAI:${message}`,
-      temperature: 0.9,
-      max_tokens: 150,
-      top_p: 1,
-      frequency_penalty: 0.0,
-      presence_penalty: 0.6,
-      stop: [" Human:", " AI:"],
-    });
-    console.info(response);
-    setAnswer(response.data.choices[0].message.content);
-    return response;
+      const data = await response.json();
+      if (response.status !== 200) {
+        throw (
+          (data.error ||
+            new Error(`Request failed with status ${response.status}`),
+          console.log(response))
+        );
+      }
+
+      setResult(data.choices[0].text);
+      setChatInput("");
+    } catch (error) {
+      // Consider implementing your own error handling logic here
+      console.error(error);
+      alert(error.message);
+    }
   }
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    getAnswer();
-    // console.log(response.data);
-    //         fetch("http://api.open-notify.org/iss-now.json")
-    //   .then(response => response.json())
-    //   .then(data => {console.log(data.message)
-    //     console.log(data.iss_position)
-    //   })
-    //   .catch(err => console.error(err));
-    setMessage("");
-  };
 
   return (
     <div>
-      <div>{answer}</div>
-      <form onSubmit={() => handleSubmit()}>
+      <div>{result}</div>
+      <form onSubmit={onSubmit}>
         <input
-          onChange={(e) => setMessage(e.target.value)}
+          onChange={(e) => setChatInput(e.target.value)}
+          name="chat"
           type="text"
-          value={message}
-          placeholder=""
+          value={chatInput}
+          placeholder="Chat with the crew!"
         />
         <button type="submit">Envoyer</button>
       </form>
